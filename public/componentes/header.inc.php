7e779bb375bce3e7fd4bbe41baa8f147bdc111a5
Marcos Lopes <marcoslopes5687@gmail.com>
2019-07-11 16:36:58 -0400

    atualização

--- public/componentes/header.inc.php
+++ public/componentes/header.inc.php
@@ -11,18 +11,22 @@
 			
 			<nav id="nav">
 				<ul>
-					<?php if($_SERVER['PATH_INFO'] != '/painel' and  $_SERVER['PATH_INFO'] != '/painel/relatorios' and $_SERVER['PATH_INFO'] != '/filtro/pesquisa/relatorio'): ?>
+					<?php if($_SERVER['PATH_INFO'] != '/painel' and  $_SERVER['PATH_INFO'] != '/painel/relatorios' and $_SERVER['PATH_INFO'] != '/filtro/pesquisa/relatorio' and $_SERVER['PATH_INFO'] != '/painel/relatorio-geral'): ?>
 						<li><a href="<?=base_url('painel') ?>">Atletas</a></li>
 					<?php endif; ?>
 					<?php if($_SERVER['PATH_INFO'] != '/usuarios'): ?>
 						<li><a href="<?=base_url('usuarios') ?>">Usuários</a></li>
 					<?php endif; ?>
+						<?php if($_SERVER['PATH_INFO'] != '/unidades'): ?>
+						<li><a href="<?=base_url('unidades') ?>">Unidades</a></li>
+					<?php endif; ?>
 					<?php if($_SERVER['PATH_INFO'] != '/instituicoes'): ?>
-						<li><a href="<?=base_url('instituicoes') ?>">Instituições</a></li>
+						<li><a href="<?=base_url('instituicoes') ?>">Escolas</a></li>
 					<?php endif; ?>
 					<?php if($_SERVER['PATH_INFO'] != '/atleta/cadastro'): ?>
 						<li><a href="<?=base_url('atleta/cadastro') ?>">Cadastrar atleta</a></li>
 					<?php endif; ?>
+					
 					<li><a href="<?=base_url('sair') ?>">Sair</a></li>
 				</ul>
 			</nav>
